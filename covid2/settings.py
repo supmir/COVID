7e11993c0a151bbf58bf4b4ddf7eaeb9cc5828a6
supmir <amir@MacBook-Air-2.local>
2020-05-18 11:38:31 +0800

    static fix.

--- covid2/settings.py
+++ covid2/settings.py
@@ -129,8 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATIC_ROOT = 'static'
-print(STATIC_ROOT)
-STATICFILES_DIRS = (
-)
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, "path/to/other/static/dir"),
+]
