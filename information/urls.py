ba386407dcb503c4d68847d69c5f5c6acb8cb59d
supmir <iskandar.amir@gmail.com>
2020-06-16 23:20:29 +0800

    Fix redirect

--- information/urls.py
+++ information/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from . import views
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('DearCovid', views.messages, name='messages'),
+    # path('DearCovid', views.messages, name='messages'),
+    path('DearCovid', RedirectView.as_view(url = "http://19-ncov.cf/DearCovid/1"), name='messages'),
     path('DearCovid/<int:page>', views.messages, name='messages'),
     # path('cc', views.cc, name='cc'),
     # path('dcc', views.dcc, name='dcc'),
