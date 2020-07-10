07a54a1284782a8fa8e24d0f4dbb5394f5407d44
supmir <iskandar.amir@gmail.com>
2020-07-11 00:59:52 +0800

    temp

--- companyProfile/views.py
+++ companyProfile/views.py
@@ -3,14 +3,12 @@ from django.shortcuts import get_object_or_404, render
 # Create your views here.
 from django.http import HttpResponse
 from .models import Product
-from django.views.decorators.clickjacking import xframe_options_exempt
 
 def index(request):
     products = Product.objects.order_by("product_name")
     context = {'products': products}
     return render(request, 'companyProfile/index.html', context)
 
-@xframe_options_exempt
 def product(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     context = {'product': product}
