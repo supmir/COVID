13254663e8770eae245979faa6729e012972124d
supmir <iskandar.amir@gmail.com>
2020-07-12 23:02:07 +0800

    temp

--- companyProfile/admin.py
+++ companyProfile/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Product
 from .models import Photos
 from .models import Videos
+from .models import Content
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Photos)
