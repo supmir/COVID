a2c729f99f1b4e3c1ec77cc3ee74b287acbab83a
supmir <iskandar.amir@gmail.com>
2021-02-19 18:37:51 +0800

    Change index site. resize fonts.

--- information/urls.py
+++ information/urls.py
@@ -5,7 +5,8 @@ from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('', RedirectView.as_view(url = "http://19-ncov.cf/DearCovid/1"), name='index'),
     # path('DearCovid', views.messages, name='messages'),
     path('DearCovid', RedirectView.as_view(url = "http://19-ncov.cf/DearCovid/1"), name='messages'),
     path('DearCovid/<int:page>', views.messages, name='messages'),
