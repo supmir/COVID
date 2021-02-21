7b587144095e573cbc0714cec144df69aa2c8e61
supmir <iskandar.amir@gmail.com>
2021-02-21 22:31:31 +0800

    fix staticfiles_dirs

--- covid2/settings.py
+++ covid2/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 # print("STATIC_ROOT",STATIC_ROOT)
 # print("STATICFILES_DIRS",STATICFILES_DIRS)
 
