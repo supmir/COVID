3b359e8ae5232d32d4cab4385345fdf6bbb634d5
Amir <iskandar.amir@gmail.com>
2021-05-17 22:44:18 +0800

    Update urls.py

--- information/urls.py
+++ information/urls.py
@@ -6,9 +6,9 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = [
     # path('', views.index, name='index'),
-    path('', RedirectView.as_view(url = "http://19-ncov.cf/DearCovid/1"), name='index'),
+    path('', RedirectView.as_view(url = "http://DearCovid.ga/DearCovid/1"), name='index'),
     # path('DearCovid', views.messages, name='messages'),
-    path('DearCovid', RedirectView.as_view(url = "http://19-ncov.cf/DearCovid/1"), name='messages'),
+    path('DearCovid', RedirectView.as_view(url = "http://DearCovid.ga/DearCovid/1"), name='messages'),
     path('DearCovid/<int:page>', views.messages, name='messages'),
     # path('cc', views.cc, name='cc'),
     # path('dcc', views.dcc, name='dcc'),
